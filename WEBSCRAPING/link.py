154c53a7c42fa57c7766cf9154a310cd61f5f99d
Juan Camilo Serrano Correa <68578543+JUNAK1@users.noreply.github.com>
2024-02-20 19:26:30 -0500

    Add files via upload
    Commit

---
+++ WEBSCRAPING/link.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# URL de la página web
+url = "https://books.toscrape.com/catalogue/"
+url1 = "https://books.toscrape.com/catalogue/page-2.html"
+
+# Realizar una solicitud GET a la página web
+print("Frist response: "+ url1)
+response = requests.get(url1)
+
+
+# Comprobar si la solicitud fue exitosa
+if response.status_code == 200:
+    # Crear un objeto BeautifulSoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Encontrar todos los elementos de libro
+    books = soup.find_all("article", class_="product_pod")
+
+    # Crear un archivo CSV para escribir los datos
+    with open("librostest2.csv", "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = ["Título", "Precio", "Rating", "Descripcion","Disponibilidad"]
+        #fieldnames = ["Título", "Precio", "Rating", "Descripcion"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
+
+        writer.writeheader()
+
+        for book in books:
+            # Extraer el título del libro
+            title = book.find("h3").find("a")["title"]
+
+            # Buscar el precio del libro
+            price_element = book.find("p", class_="price_color")
+            price = price_element.text if price_element else "Precio no disponible"
+
+            # Extraer el rating del libro
+            rating_element = book.find("p", class_="star-rating")
+            rating = rating_element["class"][1] if rating_element else "Rating no disponible"
+
+            # Acceder a la URL del libro para obtener la descripción del producto
+            book_url = url + book.find("h3").find("a")["href"]
+            print("*------------------------------------------------------------------------*")
+            print(book_url)
+            book_response = requests.get(book_url)
+
+            #book_soup = None
+            if book_response.status_code == 200:
+                print("pass")
+                book_soup = BeautifulSoup(book_response.text, "html.parser")
+                product_description = book_soup.find("meta", {"name": "description"})["content"]
+
+            else:
+                product_description = "F"
+            # Buscar la disponibilidad del libro
+            availability_element = book_soup.find("th", string="Availability")
+            availability = availability_element.find_next("td").get_text() if availability_element else "Disponibilidad no disponible"
+
+            # Escribir los detalles del libro en el archivo CSV
+            writer.writerow({
+                "Título": title,
+                "Precio": price,
+                "Rating": rating,
+                "Descripcion": product_description,
+                "Disponibilidad": availability
+            })
+
+    print("Los datos se han guardado en libros.csv")
+else:
+    print("Error al acceder a la página web")
